25ef4b26d9d059926abbe693cf48f1184780568e
reinderien <gtoombs@lakeheadu.ca>
2016-01-15 14:46:13 -0500

    Initial commit

---
+++ Fullscreen_Kongregate.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Fullscreen Kongregate
+// @description Make Kongregate cinematic mode be actually fullscreen
+// @namespace   http://github.com/reinderien
+// @downloadURL 
+// @include     http://www.kongregate.com/games/*
+// @version     0.1
+// @grant       none
+// ==/UserScript==
+
+// allow pasting
+
